687326a959c91fa0629ddb290661f5bcac38a023
Michele Beckmann <m.beckmann@live.de>
2021-12-02 17:39:07 +0100

    added Day2

---
+++ AdventOfCode.Test/2021/Day2Test.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode._2021;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode.Test._2021
+{
+    public class Day2Test
+    {
+        [Fact]
+        public void Part1Test()
+        {
+            var commands = new string[] { "forward 5",
+                "down 5",
+                "forward 8",
+                "up 3",
+                "down 8",
+                "forward 2" };
+            
+            Assert.Equal(150, Day2.SolvePart1(commands));
+        }
+        
+        [Fact]
+        public void Part2Test()
+        {
+            var commands = new string[] { "forward 5",
+                "down 5",
+                "forward 8",
+                "up 3",
+                "down 8",
+                "forward 2" };
+            
+            Assert.Equal(900, Day2.SolvePart2(commands));
+        }
+    }
+}
---
+++ AdventOfCode/2021/Day2.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2021
+{
+    public class Day2
+    {
+        public static int SolvePart1(IEnumerable<string> commands)
+        {
+            var horizontal = 0;
+            var depth = 0;
+            
+            foreach (var command in commands.Select(ParseCommand))
+            {
+                switch (command.command)
+                {
+                    case "forward":
+                        horizontal += command.parameter;
+                        break;
+                    case "down":
+                        depth += command.parameter;
+                        break;
+                    case "up":
+                        depth -= command.parameter;
+                        break;
+                }
+            }
+
+            return horizontal * depth;
+        }
+        
+        public static int SolvePart2(IEnumerable<string> commands)
+        {
+            var horizontal = 0;
+            var depth = 0;
+            var aim = 0;
+            
+            foreach (var command in commands.Select(ParseCommand))
+            {
+                switch (command.command)
+                {
+                    case "forward":
+                        horizontal += command.parameter;
+                        depth += aim * command.parameter;
+                        break;
+                    case "down":
+                        aim += command.parameter;
+                        break;
+                    case "up":
+                        aim -= command.parameter;
+                        break;
+                }
+            }
+
+            return horizontal * depth;
+        }
+
+        private static (string command, int parameter) ParseCommand(string command)
+        {
+            var commandParts = command.Split(" ");
+            return (commandParts[0], int.Parse(commandParts[1]));
+        }
+    }
+}
