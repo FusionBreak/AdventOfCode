188ac8c1d622c034f205ea8a5feaf2e71e6d726c
Michele Beckmann <m.beckmann@live.de>
2021-12-04 17:07:13 +0100

    added 2021 Day 3 Part 1

---
+++ AdventOfCode/2021/Day3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2021
+{
+    public class Day3
+    {
+        public static int SolvePart1(IEnumerable<string> inputs)
+        {
+            var gammaBits = string.Empty;
+            var epsilonBits = string.Empty;
+            var inputLenght = inputs.First().Length;
+
+            var bits = Enumerable.Range(0, inputLenght)
+                .Select(index => inputs.Select(report => report[index])
+                    .GroupBy(bit => bit)
+                    .Select(group => (group.Key, group.Count()))
+                    .OrderByDescending(group => group.Item2))
+                .ToList();
+
+            foreach (var bit in bits)
+            {
+                gammaBits += bit.First().Key;
+                epsilonBits += bit.Last().Key;
+            }
+
+            var gamma = Convert.ToInt32(gammaBits, 2);
+            var epsilon = Convert.ToInt32(epsilonBits, 2);
+            
+            return gamma * epsilon;
+        }
+
+        public static int SolvePart2(IEnumerable<string> inputs)
+        {
+            return 0;
+        }
+    }
+}
